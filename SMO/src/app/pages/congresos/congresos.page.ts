1d8339d3800b4dc4ba85ac1978c430947c39c0b3
DenyMM <deny.lolipop@gmail.com>
2019-07-12 13:27:36 -0500

    Se agregaron nuevas paginas

---
+++ SMO/src/app/pages/congresos/congresos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-congresos',
+  templateUrl: './congresos.page.html',
+  styleUrls: ['./congresos.page.scss'],
+})
+export class CongresosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
